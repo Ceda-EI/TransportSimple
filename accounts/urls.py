6bd4e27b2f31f058ded35d95e79311a9eb5bdd7c
Ceda EI <ceda_ei@webionite.com>
2025-04-09 11:19:37 +0530

    Add create user page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 from django.urls import path
 
+from accounts.views import CreateUserView
+
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("create/", CreateUserView.as_view(), name="create-user"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
 # Create your views here.
+
+class CreateUserView(FormView):
+    form_class = UserCreationForm
+    template_name = "registration/create-user.html"
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        messages.success(self.request, f"Logged in as {user.username}!")
+        return super().form_valid(form)
